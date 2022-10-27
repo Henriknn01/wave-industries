acfe71958c263768eb0be90e68f347f30a156e34
Rafixx <rafixx11@gmail.com>
2022-10-27 16:46:42 +0200

    MQTT prototype for server and client side
    Files changed:
    mqtt_publisher.py
    mqtt_client.py
    
    Files added:
    .gitignore
    
    Further work has been done on the client side and server side. Data can now be published and subscribed to. 
    
    .gitignore file added in order to streamline the commit process

--- waveBackend/BackendServices/MQTT/mqtt_subscriber.py
+++ waveBackend/BackendServices/MQTT/mqtt_subscriber.py
@@ -1,25 +1,10 @@
-import paho.mqtt.client as mqtt
+import paho.mqtt.subscribe as subscribe
 import time
 
 
-# when data is reciever, prints out the datas payload
-def on_message(client, data, message):
-    print("Recieved data_ ", str(message.payload.decode("utf-8")))
+def on_message_print(client, userdata, message):
+    print("%s %s" % (message.topic, message.payload))
 
 
-# address of the mqtt broker
-mqttBroker = "129.241.152.12"
-port = "1883"
-client = mqtt.Client("Device")
-client.connect(mqttBroker)
-
-# starts loop, subscribers to a topic. Prints out the payload of the topic subscribed.
-client.loop_start()
-client.subscribe("SUB")
-client.on_message = on_message
-time.sleep(30)
-client.loop_stop()
-
-# source: https://www.youtube.com/watch?v=kuyCd53AOtg
-# documentation used: https://www.eclipse.org/paho/index.php?page=clients/python/index.php
-
+mqttBroker = "10.22.186.196"
+subscribe.callback(on_message_print, "#", hostname=mqttBroker)
